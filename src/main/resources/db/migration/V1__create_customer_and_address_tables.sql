9103d715859e26b10ad22cbaafbc980ccd086ab7
gabrielle-lima <gaibou_lima@hotmail.com>
2026-05-02 20:42:41 -0300

    feat(db): script de criação das tabelas completo

--- src/main/resources/db/migration/V1__create_customer_and_address_tables.sql
+++ src/main/resources/db/migration/V1__create_customer_and_address_tables.sql
@@ -1,5 +1,5 @@
 CREATE TABLE customers (
-    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
     name VARCHAR(255) NOT NULL,
     birth_date DATE NOT NULL,
     sexo CHAR(1) NOT NULL,
@@ -7,6 +7,24 @@ CREATE TABLE customers (
     email VARCHAR(255) NOT NULL UNIQUE,
     phone VARCHAR(20) NOT NULL UNIQUE,
     status VARCHAR(255) NOT NULL,
-    created_at  timestamptz,
-    updated_at timestamptz
+    created_at  timestamptz NOT NULL,
+    updated_at timestamptz NOT NULL
+);
+
+
+CREATE TABLE address (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    street VARCHAR(255) NOT NULL,
+    number VARCHAR(20) NOT NULL,
+    complement VARCHAR(100),
+    neighborhood VARCHAR(100) NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    state VARCHAR(2) NOT NULL,
+    zip_code CHAR(8) NOT NULL,
+    country VARCHAR(255) NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    created_at timestamptz NOT NULL,
+    updated_at timestamptz NOT NULL,
+    customer_id int not null,
+    CONSTRAINT  fk_customer_id FOREIGN KEY (customer_id) REFERENCES customers(id)
 )
