54e681f5c35fddf19207e700efc56debc2bf80a7
Gustavo <sgkenzo212007@gmail.com>
2026-05-02 23:40:35 -0300

    refact(usecases): move address logic to CreateCustomerUseCaseImpl

--- src/main/java/br/com/customers/application/usecases/customer/impl/CreateCustomerUseCaseImpl.java
+++ src/main/java/br/com/customers/application/usecases/customer/impl/CreateCustomerUseCaseImpl.java
@@ -1,16 +1,22 @@
 package br.com.customers.application.usecases.customer.impl;
 
+import br.com.customers.api.v1.model.AddressRequestDTO;
 import br.com.customers.api.v1.model.CustomerRequestDTO;
 import br.com.customers.api.v1.model.CustomerResponseDTO;
-import br.com.customers.application.exceptions.CustomerAlreadyExistsException;
-import br.com.customers.application.usecases.address.CreateAddressUseCase;
+import br.com.customers.application.exceptions.CpfAlreadyInUseException;
+import br.com.customers.application.exceptions.EmailAlreadyInUseException;
 import br.com.customers.application.usecases.customer.CreateCustomerUseCase;
 import br.com.customers.infrastructure.adapters.outbound.repositories.CustomerRepository;
+import br.com.customers.infrastructure.adapters.outbound.repositories.entities.AddressEntity;
+import br.com.customers.infrastructure.adapters.outbound.repositories.entities.CustomerEntity;
+import br.com.customers.infrastructure.mappers.AddressMapper;
 import br.com.customers.infrastructure.mappers.CustomerMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -18,8 +24,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class CreateCustomerUseCaseImpl implements CreateCustomerUseCase {
 
     private final CustomerMapper customerMapper;
-    private final CreateAddressUseCase createAddressUseCase;
     private final CustomerRepository customerRepository;
+    private final AddressMapper addressMapper;
 
     @Override
     @Transactional
@@ -29,7 +35,7 @@ public class CreateCustomerUseCaseImpl implements CreateCustomerUseCase {
         validate(requestDTO.getCpf(), requestDTO.getEmail());
 
         var customer = customerMapper.toEntity(requestDTO);
-        var addresses = createAddressUseCase.execute(customer, requestDTO.getAddresses());
+        var addresses = createAddresses(customer, requestDTO.getAddresses());
         customer.getAddresses().addAll(addresses);
         var savedCustomer = customerRepository.save(customer);
 
@@ -41,11 +47,17 @@ public class CreateCustomerUseCaseImpl implements CreateCustomerUseCase {
 
     private void validate(String cpf, String email) {
         if (customerRepository.existsByCpf(cpf)) {
-            throw new CustomerAlreadyExistsException("field cpf already exists");
+            throw new CpfAlreadyInUseException(cpf);
         }
 
         if (customerRepository.existsByEmail(email)) {
-            throw new CustomerAlreadyExistsException("field email already exists");
+            throw new EmailAlreadyInUseException(email);
         }
+    }
+
+    private List<AddressEntity> createAddresses(CustomerEntity customer, List<AddressRequestDTO> addressesDTO) {
+        var addressesEntities = addressesDTO.stream().map(addressMapper::toEntity).toList();
+        addressesEntities.forEach(address -> address.setCustomer(customer));
+        return addressesEntities;
     }
 }
