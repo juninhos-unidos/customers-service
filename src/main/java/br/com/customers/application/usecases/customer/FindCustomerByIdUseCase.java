c025cb8b7d4743d7f444606eb8c369bc10295b71
Gustav-Stack <lucenagustavo404@gmail.com>
2026-05-07 20:19:55 -0300

    feature(customer-usecase): start findCustomerById flow

---
+++ src/main/java/br/com/customers/application/usecases/customer/FindCustomerByIdUseCase.java
@@ -0,0 +1,6 @@
+package br.com.customers.application.usecases.customer;
+import br.com.customers.api.v1.model.CustomerResponseDTO;
+
+public interface FindCustomerByIdUseCase {
+    CustomerResponseDTO execute(String id);
+}
---
+++ src/main/java/br/com/customers/application/usecases/customer/impl/FindCustomerByIdUseCaseImpl.java
@@ -0,0 +1,13 @@
+package br.com.customers.application.usecases.customer.impl;
+import br.com.customers.api.v1.model.CustomerResponseDTO;
+import br.com.customers.application.usecases.customer.FindCustomerByIdUseCase;
+
+public class FindCustomerByIdUseCaseImpl implements FindCustomerByIdUseCase {
+
+    
+
+    @Override
+    public CustomerResponseDTO execute(String id) {
+        return null;
+    }
+}
